f26fdc4ee4068bcf2fd1c512e7ba9d216ddc4389
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-21 14:28:27 +0100

    adding auth0 basic support

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,7 @@ import * as auth0 from 'auth0-js';
 @Injectable()
 export class AuthService {
 
-  var webAuth = new auth0.WebAuth({ 
+  auth0 = new auth0.WebAuth({ 
     domain:   'go-pizza42.eu.auth0.com',
     clientID: '9A_RwMFtN1RPylDgkGL7z-1LOQUS1sTP'
   });
@@ -13,7 +13,7 @@ export class AuthService {
   constructor(public router: Router) {}
 
   public login(): void {
-    webAuth.authorize({
+    this.auth0.authorize({
       connection: 'facebook'
     });
   }
