2ed95d38ff2984a24bd79cf7d80a1fbf22e7f784
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-21 22:32:08 +0100

    adding more auth0 support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: '', component: HomeComponent },
   { path: '**', redirectTo: '' }
 ];
