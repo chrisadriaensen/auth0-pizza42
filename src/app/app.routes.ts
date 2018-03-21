ab53043103dc181ffb06928a10d0aff8a8df37b0
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-21 23:29:33 +0100

    adding backend API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
