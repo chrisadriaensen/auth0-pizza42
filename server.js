294f9db7328331058e6a6d3f06b77cc46865c4ae
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-22 00:22:33 +0100

    adding backend API

--- server.js
+++ server.js
@@ -2,9 +2,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+var jwtCheck = jwt({
+   
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://go-pizza42.eu.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'http://pizza42-auth0.herokuapp.com/api/',
+  issuer: 'https://go-pizza42.eu.autho.com/',
+  algorithms: ['RS256']
+
+});
+
+app.use('/api/*', jwtCheck);
 
 app.get('/api/order', function(req, res) {
   
