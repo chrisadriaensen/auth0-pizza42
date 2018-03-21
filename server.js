01613490bfb0e8a68e84a110b4b0e0aff005c7d5
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-21 22:58:33 +0100

    adding backend API

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 //Install express server
 const express = require('express');
 const app = express();
+const jwt = require('express-jwt');
+const rsaValidation = require('auth0-api-jwt-rsa-validation');
 
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/orders', function(req, res){
+  var orders = [
+    { id: 1, type: 'Margherita'},
+    { id: 2, type: 'Quatro Fromaggio'}
+  ];
+
+  res.json(orders);
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
