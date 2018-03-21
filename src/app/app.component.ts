ab741eb71c0df9c5cecb0326692f0685943e1fac
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-22 00:51:32 +0100

    adding backend API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth/auth.service';
 
 @Component({
@@ -6,11 +7,30 @@ import { AuthService } from './auth/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Pizza 42';
 
-  constructor(public auth: AuthService) {
-    auth.handleAuthentication();
+  var apiToken = any;
+  
+  constructor(public auth: AuthService, public http: HttpClient) {
+    this.auth.handleAuthentication();
+  }
+
+  ngOnInit(): void {
+
+    this.http.post('https://go-pizza42.eu.auth0.com/oauth/token', {
+
+        client_id: '3nEw4IGHoTUJpIfUPKT24zDNGRuZvLtK',
+        client_secret: 'kq4X6PEvzl4bLVIWtLQsoEyUxLyfZ0-PABDs0xk6n7uengrcZJDLqxDEHTduPTea',
+        audience: 'http://pizza42-auth0.herokuapp.com/api/',
+        grant_type: 'client_credentials'
+
+      }).subscribe(
+
+        res => { apiToken = res.access_token; }
+
+    );
+
   }
 
 }
