35b8043d543873e355130df063dfbc999f54f6ab
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-22 12:09:09 +0100

    improvements

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
     responseType: 'token id_token',
     audience: 'http://pizza42-auth0.herokuapp.com/api/',
     redirectUri: 'http://pizza42-auth0.herokuapp.com',
-    scope: 'openid profile read:orders place:order'
+    scope: 'openid profile order'
   });
   
   userProfile: any;
