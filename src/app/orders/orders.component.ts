5dada9bbcc5c3cf180ee757d2e5d994718b169b1
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-22 02:27:38 +0100

    adding backend API

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
-export class OrdersComponent {}
+export class OrdersComponent {
+
+  orders: any;
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit() {
+   
+    this.http.get('http://pizza42-auth0.herokuapp.com/api/orders', {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access_token'))
+    }).pipe(
+      map(res => res.json(),
+      catchError(error => console.log(error))
+    ).subscribe(
+      data => this.orders = data.message,
+      error => this.orders = error
+    );
+
+  }
+
+}
