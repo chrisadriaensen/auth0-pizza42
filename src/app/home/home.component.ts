bea469f3b2c192dbd6e2027deab459bfea20c580
Chris Adriaensen <chris.adriaensen@gmail.com>
2018-03-23 11:59:19 +0100

    improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,9 +17,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
 
     this.accessToken = localStorage.getItem('access_token');
-    if (this.accessToken) this.accessToken = atob(this.accessToken);
     this.idToken = atob(localStorage.getItem('id_token'));
-    if (this.idToken) this.idToken = atob(this.idToken);
     this.expiresAt = localStorage.getItem('expires_at');   
     this.scopes = localStorage.getItem('scopes');
 
